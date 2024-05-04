4d5d4f74fb3b6102d67992a55682226184a80fb6
rimao416 <omarkayumba12345@gmail.com>
2024-05-04 21:26:50 +0100

    Push

--- src/variants.tsx
+++ src/variants.tsx
@@ -1,20 +1,25 @@
-export const fadeIn = (direction: "up" | "down" | "left" | "right", delay: number) => {
-    return {
-      hidden: {
-        y: direction === "up" ? 80 : direction === "down" ? -80 : 0,
-        opacity: 0,
-        x: direction === "left" ? 80 : direction === "right" ? 80 : 0,
+export const fadeIn = (
+  direction: "up" | "down" | "left" | "right",
+  delay: number
+) => {
+  return {
+    hidden: {
+      y: direction === "up" ? 80 : direction === "down" ? -80 : 0,
+      opacity: 0,
+      x: direction === "left" ? 80 : direction === "right" ? 80 : 0,
+    },
+    show: {
+      x: 0,
+      y: 0,
+      opacity: 1,
+      transition: {
+        type: "tween",
+        duration: 1.2,
+        delay: delay,
+        //   ease: "easeOut",
+        //   ease: [0.6, 0.01, -0.05, 0.9],
+        ease: [0.25, 0.25, 0.25, 0.85],
       },
-      show: {
-        x: 0,
-        y: 0,
-        opacity: 1,
-        transition: {
-          type: "tween",
-          duration: 1.2,
-          delay: delay,
-          ease: [0.25, 0.46, 0.45, 0.94],
-        },
-      },
-    };
+    },
   };
+};
